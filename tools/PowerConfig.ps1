eb625ca5d493798e0dfa815db3ab87cc18d54543
systematicguy <david.h@systematicguy.com>
2022-10-27 17:46:26 +0200

    turn on hibernate option

---
+++ tools/PowerConfig.ps1
@@ -0,0 +1,38 @@
+. $PSScriptRoot\..\windows\Environment.ps1
+if ($AlreadySourced[$PSCommandPath] -eq $true) { return } else { $AlreadySourced[$PSCommandPath] = $true }
+
+Configuration PowerConfig
+{
+    Import-DscResource -ModuleName PSDesiredStateConfiguration
+    Import-DscResource -ModuleName GroupPolicyDsc
+
+    Node "localhost"
+    {
+        Registry "HibernateEnabled"  
+        {
+            Key       = "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Power"
+            ValueName = "HibernateEnabled"
+            ValueType = "Dword"
+            ValueData = 1
+        }
+
+        Registry "System_ShowHibernateOption"  
+        {
+            Key       = "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\System"
+            ValueName = "ShowHibernateOption"
+            ValueType = "Dword"
+            ValueData = 1
+        }
+
+        Registry "Explorer_ShowHibernateOption"  
+        {
+            Key       = "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\Explorer"
+            ValueName = "ShowHibernateOption"
+            ValueType = "Dword"
+            ValueData = 1
+        }
+    }
+}
+
+PowerConfig -Output $DscMofDir\PowerConfig -ConfigurationData $DscConfigPath
+Start-DscConfiguration -Path $DscMofDir\PowerConfig -Wait -Force -Verbose
