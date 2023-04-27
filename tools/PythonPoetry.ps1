d4bf719e1285b9c932e4e3cba71a5b9739d99b43
systematicguy <david.h@systematicguy.com>
2023-04-27 16:35:02 +0200

    refactor: simple poetry installation using pipx

--- tools/PythonPoetry.ps1
+++ tools/PythonPoetry.ps1
@@ -3,7 +3,7 @@ if ($AlreadySourced[$PSCommandPath] -eq $true) { return } else { $AlreadySourced
 
 . $RepoRoot\windows\UserCredential.ps1
 . $RepoToolsDir\PowershellConfig.ps1
-. $RepoToolsDir\Pyenv.ps1
+. $RepoToolsDir\Pipx.ps1
 
 $desiredPoetryVersion = $UserConfig.Python.PoetryVersion
 $desiredPathElement = "$UserDir\AppData\Roaming\Python\Scripts"
@@ -26,15 +26,11 @@ Configuration PythonPoetry
             SetScript = {
                 $desiredPoetryVersion = $using:desiredPoetryVersion
                 if ($desiredPoetryVersion -ne "") {
-                    $env:POETRY_VERSION = $desiredPoetryVersion
+                    $versionedPoetry = "poetry==$desiredPoetryVersion"
+                } else {
+                    $versionedPoetry = "poetry"
                 }
-                $proxyCredential = $using:UserCredentialAtAd
-                $webClient = New-Object System.Net.WebClient
-                $webClient.Proxy.Credentials = $proxyCredential.GetNetworkCredential()
-                $webClient.DownloadString("https://install.python-poetry.org") | python | Out-File $using:outputFile -Encoding ASCII -Append
-                if ($LASTEXITCODE -ne 0) {
-                    throw "Exited with $LASTEXITCODE"
-                }
+                pipx install $versionedPoetry
             }
             TestScript = {
                 try {
@@ -45,49 +41,6 @@ Configuration PythonPoetry
                 $foundPoetryVersionString -ne $null  # emit $true if already installed
             }
         }
-
-        Script PoetryOnPath
-        {
-            DependsOn            = "[Script]InstallPoetry"
-            PsDscRunAsCredential = $UserCredentialAtComputerDomain
-
-            GetScript = {
-                #Do Nothing
-            }
-            SetScript = {
-                $desiredPathElement = $using:desiredPathElement
-                $newPath = [Environment]::GetEnvironmentVariable("PATH", "User") + [IO.Path]::PathSeparator + $desiredPathElement
-                [Environment]::SetEnvironmentVariable("PATH", $newPath, "User")
-            }
-            TestScript = {
-                $desiredPathElement = $using:desiredPathElement
-                [System.Environment]::GetEnvironmentVariable('PATH', 'User').ToLower().Split([IO.Path]::PathSeparator).Contains($desiredPathElement.ToLower())
-            }
-        }
-
-        # TODO: implement version parsing, and uncomment the next:
-        # Script UpdatePoetry
-        # {
-        #     PsDscRunAsCredential = $UserCredentialAtComputerDomain
-        #     GetScript = {
-        #         #Do Nothing
-        #     }
-        #     SetScript = {
-        #         $desiredPoetryVersion = $using:desiredPoetryVersion
-        #         poetry self update $using:desiredPoetryVersion | Out-File $using:outputFile -Encoding ASCII -Append
-        #         if ($LASTEXITCODE -ne 0) {
-        #             throw "Exited with $LASTEXITCODE"
-        #         }
-        #     }
-        #     TestScript = {
-        #         $desiredPoetryVersion = $using:desiredPoetryVersion
-        #         $foundPoetryVersion = (poetry --version)
-        #         $foundPoetryVersion -eq $desiredPoetryVersion  # emit $true if already the same
-        #     }
-        # }
-
-        # uninstalling: iwr "https://install.python-poetry.org" -UseBasicParsing | python - --uninstall
-        # or delete these: AppData\Roaming\pypoetry, AppData\Roaming\Python\Scripts\poetry.exe
     }
 }
 
