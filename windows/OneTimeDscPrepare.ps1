7393ec8245bbb6564e0bf4f811234cc2ea0ed8e9
systematicguy <david.h@systematicguy.com>
2023-06-16 18:55:30 +0200

    churn: better name for OneTimeDscPrepare.ps1

--- windows/OneTimeDscPrepare.ps1
+++ windows/OneTimeDscPrepare.ps1
@@ -4,7 +4,12 @@
 Set-WinSystemLocale en-US
 
 # was needed on my own laptop, some corporate machines have it already:
-Set-WsManQuickConfig -Force
+try {
+    Set-WSManQuickConfig -Force
+} catch {
+    Write-Host "You can probably ignore this error. Most machines need Set-WSManQuickConfig."
+    Write-Host "In some corporate setup it is not needed any more, but you never know beforehand and those in turn might fail if you try to perform this."
+}
 
 # maybe needed:
 #Install-Module -Name PSDscResources -Force
@@ -16,6 +21,7 @@ Install-Module -Name xPSDesiredStateConfiguration -Force
 Install-Module -Name ComputerManagementDsc -Force
 Install-Module -Name PowerShellModule -Force
 Install-Module -Name xPowerShellExecutionPolicy -Force
+Install-Module -Name DSCR_AppxPackage -Force
 Install-Script -Name ConvertTo-Expression -Force
 
 . $PSScriptRoot\DscSetupCert.ps1
