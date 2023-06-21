7eed8b1a3eff2e91ef83be4357d695b11cb33d8b
systematicguy <david.h@systematicguy.com>
2023-06-21 21:13:01 +0200

    refactor: remove unnecessary ; and add todos

--- tools/TotalCommander.ps1
+++ tools/TotalCommander.ps1
@@ -14,55 +14,55 @@ $totalCmdPluginDir = "$UserBinDir\total_commander\plugins"
 # https://www.ghisler.ch/wiki/index.php?title=Wincmd.ini
 $totalCmdIniConfig = @{
     Configuration = @{
-        Editor                    = "`"$VsCodeExePath`" `"%1`"";
-        DarkMode                  = "1";
-        RenameSelOnlyName         = "1";
-        AltSearch                 = "3";
-        QuickSearchMatchBeginning = "0";
-        QuickSearchExactMatch     = "0";
-        SizeStyle                 = "3";
-        SizeFooter                = "3";
-        SoundDelay                = "-10";
-        VerifyCopy                = "1";
-        ShowHiddenSystem          = "1";
-        QuickSearchAutoFilter     = "1";
-        UseLongNames              = "1";
-        IconOverlays              = "1";
-        DirTabOptions             = "1945";
-        DirTabRevert              = "1";
-        DirTabFilters             = "1";
-        WatchDirs                 = "51";
-        PluginBaseDir             = $totalCmdPluginDir;
-        # AutoInstallPlugins        = "1";
-    };
+        Editor                    = "`"$VsCodeExePath`" `"%1`""
+        DarkMode                  = "1"
+        RenameSelOnlyName         = "1"
+        AltSearch                 = "3"
+        QuickSearchMatchBeginning = "0"
+        QuickSearchExactMatch     = "0"
+        SizeStyle                 = "3"
+        SizeFooter                = "3"
+        SoundDelay                = "-10"
+        VerifyCopy                = "1"
+        ShowHiddenSystem          = "1"
+        QuickSearchAutoFilter     = "1"
+        UseLongNames              = "1"
+        IconOverlays              = "1"
+        DirTabOptions             = "1945"
+        DirTabRevert              = "1"
+        DirTabFilters             = "1"
+        WatchDirs                 = "51"
+        PluginBaseDir             = $totalCmdPluginDir
+        # AutoInstallPlugins        = "1"
+    }
     Confirmation = @{
-        deleteDirs        = "0";
-        OverwriteFiles    = "1";
-        OverwriteReadonly = "0";
-        OverwriteHidSys   = "0";
-        MouseActions      = "1";
-    };
+        deleteDirs        = "0"
+        OverwriteFiles    = "1"
+        OverwriteReadonly = "0"
+        OverwriteHidSys   = "0"
+        MouseActions      = "1"
+    }
     Tabstops = @{
         AdjustWidth = "1";
-    };
+    }
     Layout = @{
-        ButtonBar             = "1";
-        ButtonBarVertical     = "1";
-        DriveBar1             = "1";
-        DriveBar2             = "1";
-        DriveBarFlat          = "1";
-        InterfaceFlat         = "1";
-        DriveCombo            = "1";
-        DirectoryTabs         = "1";
-        XPthemeBg             = "1";
-        CurDir                = "1";
-        TabHeader             = "1";
-        StatusBar             = "1";
-        CmdLine               = "1";
-        KeyButtons            = "1";
-        HistoryHotlistButtons = "1";
-        BreadCrumbBar         = "1";
-    };
+        ButtonBar             = "1"
+        ButtonBarVertical     = "1"
+        DriveBar1             = "1"
+        DriveBar2             = "1"
+        DriveBarFlat          = "1"
+        InterfaceFlat         = "1"
+        DriveCombo            = "1"
+        DirectoryTabs         = "1"
+        XPthemeBg             = "1"
+        CurDir                = "1"
+        TabHeader             = "1"
+        StatusBar             = "1"
+        CmdLine               = "1"
+        KeyButtons            = "1"
+        HistoryHotlistButtons = "1"
+        BreadCrumbBar         = "1"
+    }
     searches = @{
         not_older_than_1_hour_SearchFor   = ""
         not_older_than_1_hour_SearchIn    = "c:\"
@@ -86,7 +86,6 @@ $totalCmdIniConfig = @{
         menu4 = "Registry"
         cmd4  = "cd \\\Registry"
     }
-
 }
 
 Configuration TotalCommanderInstallation
@@ -149,18 +148,18 @@ $contentPluginSettings = @{
         "0_flags"  = "0"
     }
     CustomFields = @{
-        Titles      = "Pictures|Videos";
-        AutoLoad    = "1";
+        Titles      = "Pictures|Videos"
+        AutoLoad    = "1"
 
-        Widths1     = "181,30,-80,80,62,50,80";
-        Headers1    = "Size\nCreation\nModDate\nCamMaker\nCamModel";
-        Contents1   = "[=tc.size]\n[=tc.creationdate]\n[=tc.writedate]\n[=exif.Make]\n[=exif.Model]";
-        Options1    = "-1|0|96";
+        Widths1     = "181,30,-80,80,62,50,80"
+        Headers1    = "Size\nCreation\nModDate\nCamMaker\nCamModel"
+        Contents1   = "[=tc.size]\n[=tc.creationdate]\n[=tc.writedate]\n[=exif.Make]\n[=exif.Model]"
+        Options1    = "-1|0|96"
 
-        Widths2     = "181,30,-80,80,62";
-        Headers2    = "Size\nCreation\nModDate";
-        Contents2   = "[=tc.size]\n[=tc.creationdate]\n[=tc.writedate]";
-        Options2    = "-1|0|96";
+        Widths2     = "181,30,-80,80,62"
+        Headers2    = "Size\nCreation\nModDate"
+        Contents2   = "[=tc.size]\n[=tc.creationdate]\n[=tc.writedate]"
+        Options2    = "-1|0|96"
     }
 }
 Configuration TCmdContentPlugins
@@ -222,5 +221,27 @@ Configuration TCmdFileSystemPlugins
     }
 }
 ApplyDscConfiguration "TCmdFileSystemPlugins"
+
+#TODO checkout:
+# packer plugins
+#  7zip https://ghisler.fileburst.com/plugins/wcx_7zip.zip
+#  GIF https://ghisler.fileburst.com/plugins/gifwcx.zip
+#  ISO https://ghisler.fileburst.com/plugins/iso_plugin.zip
+#
+# file system plugins
+#  Back2Life https://ghisler.fileburst.com/fsplugins/b2l4tc.zip ($10) restore erased files from FAT and NTFS
+#  PROC https://www.diskinternals.com/download/diskinternals_procfs.zip process explorer with window detection
+#  SFTP https://www.totalcommander.ch/win/fs/sftpplug.zip
+#  WebDAV https://ghisler.fileburst.com/fsplugins/webdav.zip
+#
+# lister plugins
+#  fileinfo http://fg.tcplugins.free.fr/fileinfo.htm Version, dlls, etc
+#  Imagine https://ghisler.fileburst.com/lsplugins/wlx_imagine_x64.zip GIF viewer, etc.
+#  SQLiteViewer https://ghisler.fileburst.com/progman13/wlx_SQLiteViewer.zip
+#
+# content plugins
+#  MIME Info https://ghisler.fileburst.com/progman13/wdx_MIMEInfo.zip
+#  ShellDetails https://ghisler.fileburst.com/content/wdx_shelldetails.zip all Windows Explorer fields
+#  xPDFSearch https://ghisler.fileburst.com/content/wdx_xpdfsearch.zip
 
 LogTodo -Message "Total Commander activation: place the wincmd.key file into the installation dir"
