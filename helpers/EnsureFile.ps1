0c61d8bac1fae512ff4616dda54b2ef37a5e7185
systematicguy <david.h@systematicguy.com>
2023-06-23 12:23:39 +0200

    fix: EnsureFile encoding parameter will be a string

--- helpers/EnsureFile.ps1
+++ helpers/EnsureFile.ps1
@@ -3,19 +3,22 @@ function EnsureFile {
         [Parameter(Mandatory=$true)]
         [string]$Path,
         
-        [System.Text.Encoding]$EncodingIfMissing = $null
+        [string]$EncodingIfMissing = $null
     )
 
     $parentPath = Split-Path -Path $Path -Parent
     if (-not (Test-Path -Path $parentPath)) {
+        Write-Host "Creating parent directory [$parentPath]..."
         New-Item -Path $parentPath -ItemType Directory -Force
     }
     
     if (-not (Test-Path -Path $Path)) {
         if ($EncodingIfMissing -eq $null) {
+            Write-Host "Creating file [$Path]..."
             New-Item -Path $Path -ItemType File
         }
         else {
+            Write-Host "Creating file [$Path] with encoding [$EncodingIfMissing]..."
             $null | Set-Content -Path $Path -Encoding $EncodingIfMissing
         }
     }
