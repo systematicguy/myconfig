62d7e48b4ddd63eb6dd5b579003d572269125abe
systematicguy <david.h@systematicguy.com>
2023-08-16 17:18:15 +0200

    refactor: use EnsureChocoPackage for docker-desktop

--- tools/Docker.ps1
+++ tools/Docker.ps1
@@ -1,45 +1,22 @@
 . $PSScriptRoot\..\windows\Environment.ps1
 if ($AlreadySourced[$PSCommandPath] -eq $true) { return } else { $AlreadySourced[$PSCommandPath] = $true }
 
-. $RepoRoot\helpers\UserCredential.ps1
+. $RepoRoot\helpers\Chocolatey.ps1
 . $RepoRoot\helpers\EnsureFile.ps1
 . $RepoRoot\helpers\Json.ps1
 
-. $RepoToolsDir\Chocolatey.ps1
-. $RepoToolsDir\Jq.ps1
 . $RepoToolsDir\Wsl.ps1
 
 $dockerSettingsPath = "$UserDir\AppData\Roaming\Docker\settings.json"
 
-Configuration DockerDesktop
-{
-    Import-DscResource -ModuleName PSDesiredStateConfiguration
-    Import-DscResource -ModuleName cChoco
-    Import-DscResource -ModuleName DSCR_FileContent
-    
-    Node "localhost"
-    {
-        cChocoPackageInstaller DockerDesktop
-        {
-            PsDscRunAsCredential = $UserCredential
+EnsureChocoPackage -Name "docker-desktop"
 
-            Name = "docker-desktop"
-        }
-    }
-}
-ApplyDscConfiguration "DockerDesktop"
-
-
-# ==============================================================================================
-# settings.json configuration
 # docker desktop works with settings.json
 #  it fills in missing settings on startup, doesn't overwrite existing ones
 #  it adjusts settingsVersion upon startup
 #  it doesn't save on simple exit, but does not play nice with settings.json edited while running
 #  it crashes if settingsVersion key is missing, but is ok with a value of 0
-$userDockerConfig = $UserConfig.DockerDesktop['settings.json']
 $dockerFallbackSettingsVersion = $UserConfig.DockerDesktop.fallbackSettingsVersion
-Write-Host "dockerFallbackSettingsVersion: $dockerFallbackSettingsVersion"
 EnsureFile `
     -Path $dockerSettingsPath `
     -EncodingIfMissing ASCII `
