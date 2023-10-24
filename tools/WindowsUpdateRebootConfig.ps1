5ae019a0ff4f25a810335eea2e78122a6b05cb3f
David Horvath <david.horvath@amanox.com>
2023-10-24 03:28:40 +0200

    fix: registry key for update reboot config

--- tools/WindowsUpdateRebootConfig.ps1
+++ tools/WindowsUpdateRebootConfig.ps1
@@ -5,7 +5,7 @@ if ($AlreadySourced[$PSCommandPath] -eq $true) { return } else { $AlreadySourced
 
 EnsureRegistry -Purpose "DisableAutomaticReboot" -RegistryConfig @{
     # https://superuser.com/questions/957267/how-to-disable-automatic-reboots-in-windows-10
-    "\\\Registry\HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" = @{
+    "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" = @{
         # AUOptions = 0x00000002  # Notify before downloading and installing any updates.
         AUOptions = 0x00000003  # Download the updates automatically and notify when they are ready to be installed
         # AUOptions = 0x00000004  # Automatically download updates and install them on the schedule (install time-window not configured!)
